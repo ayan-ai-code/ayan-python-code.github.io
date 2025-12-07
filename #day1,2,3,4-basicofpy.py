7abe90e38534784a5658962f8b6c51361937eec4
Ayan Akhtar Ansari <akhterayan212@gmail.com>
2025-12-07 17:53:03 +0545

    Add files via upload

---
+++ #day1,2,3,4-basicofpy.py
@@ -0,0 +1,18 @@
+#day1,2,3,4
+
+#string
+print("hello world")
+#None type
+print(None)
+#integer
+print(5)
+#float
+print(5.2)
+#arthemitic operators (/*-+)
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+#this is normal division it output will be float (ex:5.0)
+print(10 / 2)
+#if we want to get integer output then we have to use double slash called floor divison (ex:5)
+print(10 // 2)
