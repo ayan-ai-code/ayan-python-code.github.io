5038f1a09f7f94f6ce9646d361e5aeda5749de1b
Ayan Akhtar Ansari <akhterayan212@gmail.com>
2026-05-13 19:00:55 +0545

    Add files via upload

---
+++ #day7.py
@@ -0,0 +1,14 @@
+#day7
+
+#math operator
+print(5 + 2)  #add            '+'
+print(5 - 2)  #subtract       '-'
+print(5 * 2)  #multiply       '*'
+print(15 / 2)  #divide          '/'
+print(15 // 2)  #floor division '//'
+print(10 % 6)  #modulus         '%'(gives remainder)
+print(10**2)  #exponential    '**'(give power)
+
+#HOMEWORK -
+#CREATE A CALCULATOR CAPABLE OF PERFORMING ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, FLOOR DIVISION, MODULUS AND EXPONENTIATION ON TWO NUMBERS TAKEN AS INPUT FROM THE USER. YOUR PROGRAM SHOULD FORMAT THE OUTPUT IN A READABLE MANNER.
+#in day 8
