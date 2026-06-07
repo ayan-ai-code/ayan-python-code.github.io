6df2f463b4bf0c49c0647c97a9fe5dd5a98fd2db
Ayan Akhtar Ansari <akhterayan212@gmail.com>
2026-06-07 16:47:07 +0545

    Add files via upload

---
+++ #day8.py
@@ -0,0 +1,14 @@
+#day8
+#(h/w)
+
+a=5
+b=2
+print(("a=5 \nb=2" ))
+print("the vale of a+b is",a+b)
+print("the vale of a-b is",a-b)
+print("the vale of a*b is",a*b)
+print("the vale of a/b is",a/b)
+print("the vale of a%b is",a%b)
+print("the vale of a**b is",a**b)
+
+#completed homework
